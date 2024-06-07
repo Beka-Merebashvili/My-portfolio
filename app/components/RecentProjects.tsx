6f3d46fc0815b2b7fd86ee57bb2eabdedfd02853
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-06-07 14:04:15 +0400

    fix pin container style

--- app/components/RecentProjects.tsx
+++ app/components/RecentProjects.tsx
@@ -4,6 +4,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
+import Link from "next/link";
 
 const RecentProjects = () => {
   return (
@@ -19,8 +20,7 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              title={item.title}
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
@@ -66,9 +66,9 @@ const RecentProjects = () => {
                 </div>
 
                 <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
-                  </p>
+                  <Link href={item.link} target="_blank" className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site 
+                  </Link>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
               </div>
