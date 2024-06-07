846caafc20b2851e8532907b21e332851a1ccf57
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-06-07 12:17:44 +0400

    fix navbar and footer bugs

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,14 +7,6 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
-
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
@@ -36,7 +28,7 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
+        <p className="md:text-base text-sm md:font-normal font-light mb-4 md:mb-0">
           Copyright © 2024 Beka Merebashvili
         </p>
 
