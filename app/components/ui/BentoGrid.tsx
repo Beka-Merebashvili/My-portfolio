5c9e676604723ddb798531330135d8571d241510
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2024-06-10 20:29:37 +0400

    remove globe

--- app/components/ui/BentoGrid.tsx
+++ app/components/ui/BentoGrid.tsx
@@ -125,7 +125,6 @@ export const BentoGridItem = ({
             {title}
           </div>
 
-          {/* {id === 2 && <GridGlobe /> } */}
 
           {/* Tech stack list div */}
           {id === 3 && (
